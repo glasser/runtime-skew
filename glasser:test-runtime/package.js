b9b4522e403e2a86dd5e36fffe9c200e79548a2b
David Glasser <glasser@davidglasser.net>
2014-09-04 23:08:22 -0700

    Initial

---
+++ glasser:test-runtime/package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  summary: " \* Fill me in! *\ ",
+  version: "1.0.0",
+  git: " \* Fill me in! *\ "
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0.1');
+  api.use('templating');
+  api.addFiles('foo.html');
+});
